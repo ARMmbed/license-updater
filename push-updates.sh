98aca325cee09a39bb965f62921f90657f98ca0b
sarahmarshy <sarah.marsh.engineer@gmail.com>
2018-03-28 15:16:08 -0500

    Remove adding remote in shell script

--- push-updates.sh
+++ push-updates.sh
@@ -2,20 +2,18 @@
 
 update_license()
 {
-    remote="https://github.com/ARMmbed/$1"
     dir=$1
     fork_remote="https://github.com/$2/$1"
-    echo "Cloning $remote"
-    git clone $remote
+    echo "Cloning $fork_remote"
+    git clone $fork_remote
     python update-license.py -d $dir -f c_header_license.txt --ftype .c .h .cpp .js 
     python update-license.py -d $dir -f python_header_license.txt --ftype .py
     cd $dir
     cat ../LICENSE > LICENSE
-    git remote add my_fork $fork_remote
     git checkout -b 'license-test'
     git add -A 
     git commit -m "Add Apache licenses"
-    git push my_fork -u license-test
+    git push origin -u license-test
     cd ..
 }
 
